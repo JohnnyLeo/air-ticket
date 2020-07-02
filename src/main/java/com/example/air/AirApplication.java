97dbb2919e4179252c4d122d56f775cfd355adc0
Johnny Leo <997510177@qq.com>
2020-07-02 20:25:52 +0800

    create project

---
+++ src/main/java/com/example/air/AirApplication.java
@@ -0,0 +1,15 @@
+package com.example.air;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jleo
+ * @date 2020/7/2
+ */
+@SpringBootApplication
+public class AirApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirApplication.class, args);
+    }
+}
